7e80b8b916093d72dd736b7167b91f9110fe016d
fraramfra <96029973+fraramfra@users.noreply.github.com>
2025-02-27 09:37:05 -0800

    Updating debug option to False (#26)

--- src/app.py
+++ src/app.py
@@ -274,4 +274,4 @@ def update_charts(selected_cd, selected_modes, time_range):
 ### --- RUN THE APP ---
 
 if __name__ == "__main__":
-    app.server.run(port=8000, host="127.0.0.1", debug=True)
+    app.server.run(port=8000, host="127.0.0.1", debug=False)
