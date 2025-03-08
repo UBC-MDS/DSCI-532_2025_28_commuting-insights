5451d7fd2425719c123c52b5b1c1c86e3a897646
Francisco Ramirez <fraramfra@gmail.com>
2025-03-07 20:29:23 -0800

    Creating update_mode.py for first callback

--- src/app.py
+++ src/app.py
@@ -16,6 +16,7 @@ parent_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(parent_dir)
 
 from src.data.preprocessing import load, widget_inputs
+from src.callbacks.update_mode import update_mode_callback
 
 ### --- LOAD AND PREPROCESS DATA ---
 
@@ -27,21 +28,7 @@ alt.data_transformers.enable("vegafusion")
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.CERULEAN], title="Commuting Insights Dashboard")
 server = app.server
 
-@app.callback(
-    Output("mode-dropdown", "options"),
-    [Input("cd-dropdown", "value")]
-)
-def update_mode_options(selected_cd):
-    if not selected_cd:
-        return dropdown_options
-    # Filter for the selected CD and nonzero average commute time
-    df_cd = df[(df["GEO"] == selected_cd) & (df["AverageCommuteTime"] > 0)]
-    # Get the unique modes that appear in the CD
-    modes = df_cd["Main mode of commuting (21)"].unique()
-    # Only keep those modes that are in the allowed available_modes list
-    valid_modes = [m for m in modes if m in available_modes]
-    options = [{"label": m, "value": m} for m in valid_modes]
-    return options
+update_mode_callback(df, available_modes, dropdown_options)
 
 @app.callback(
     Output("cd-dropdown", "options"),
---
+++ src/callbacks/update_mode.py
@@ -0,0 +1,18 @@
+from dash import Input, Output, callback
+
+def update_mode_callback(df, available_modes, dropdown_options):
+    @callback(
+        Output("mode-dropdown", "options"),
+        [Input("cd-dropdown", "value")]
+    )
+    def update_mode_options(selected_cd):
+        if not selected_cd:
+            return dropdown_options
+        # Filter for the selected CD and nonzero average commute time
+        df_cd = df[(df["GEO"] == selected_cd) & (df["AverageCommuteTime"] > 0)]
+        # Get the unique modes that appear in the CD
+        modes = df_cd["Main mode of commuting (21)"].unique()
+        # Only keep those modes that are in the allowed available_modes list
+        valid_modes = [m for m in modes if m in available_modes]
+        options = [{"label": m, "value": m} for m in valid_modes]
+        return options
