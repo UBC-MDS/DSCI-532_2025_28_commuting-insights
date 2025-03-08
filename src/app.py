5b44b4d134ec3a7999769d3c5b7e76a836196a63
Jinxiong You <jinxiongyou@outlook.com>
2025-03-07 19:36:03 -0800

    change debug

--- src/app.py
+++ src/app.py
@@ -619,4 +619,4 @@ def update_charts(selected_cd, selected_modes, time_range):
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 8050))
-    app.run_server(host="0.0.0.0", port=port, debug=True)
+    app.run_server(host="0.0.0.0", port=port, debug=False)
