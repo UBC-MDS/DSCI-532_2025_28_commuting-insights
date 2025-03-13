c60f107d5ed0ce7ef8653c4aad874b80a21029fe
Francisco Ramirez <fraramfra@gmail.com>
2025-03-13 16:51:22 -0700

    Loading binary files and running app.

--- src/app.py
+++ src/app.py
@@ -32,8 +32,8 @@ from src.components.title_and_footer import create_title, create_footer
 #)
 
 geojson_data, df = load(
-    "data/processed/binary/data_geojson", 
-    "data/processed/binary/data_csv"
+    "data/processed/binary/data_geojson.parquet", 
+    "data/processed/binary/data_csv.parquet"
 )
 
 provinces, dropdown_province_options, available_modes, dropdown_options, available_cdnames, dropdown_cd_options, time_bins, time_bin_order, slider_marks = widget_inputs(df)
